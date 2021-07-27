bead94ec42e98827b5f2eb4131f6dc3fd27a9673
Marco Rezkallah <mmsrezkallah@gmail.com>
2021-07-27 10:18:20 +0200

    add manual route table

--- route-tables.tf
+++ route-tables.tf
@@ -49,3 +49,15 @@ resource "aws_route_table_association" "online-private-b" {
   route_table_id = aws_route_table.private.id
 }
 
+# resource "aws_route_table" "manual" {
+#   vpc_id = aws_vpc.main.id
+
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.main-igw.id
+#   }
+
+#   tags = {
+#     Name = "${var.project}-${var.environment}-Manual-RT"
+#   }
+# }
